9a3293056f0fc7bb6b33b36f2c681d1a53690fab
stefano <stefano.vannucchi1@gmail.com>
2023-06-10 12:27:48 +0200

    computeZKBuond

--- src/main/java/it/unifi/swe4es/sv/cptask/services/CpTaskService.java
+++ src/main/java/it/unifi/swe4es/sv/cptask/services/CpTaskService.java
@@ -70,4 +70,106 @@ public class CpTaskService {
     // return C(S(vSource))
     return computeWCET(subgraphs.get(source));
   }
+
+  public Integer computeZKBuond(GraphDTO graph, Integer m) {
+    final List<NodeDTO> reversedTopologicalSort = new ArrayList<>(graphService.topologicalSort(graph));
+    Collections.reverse(reversedTopologicalSort);
+
+    Map<NodeDTO, Set<NodeDTO>> s = new HashMap<>();
+    Map<NodeDTO, Set<NodeDTO>> t = new HashMap<>();
+    Map<NodeDTO, Integer> f = new HashMap<>();
+
+    NodeDTO sink = reversedTopologicalSort.get(0);
+    NodeDTO source = reversedTopologicalSort.get(reversedTopologicalSort.size() - 1);
+
+    s.put(sink, Stream.of(sink).collect(Collectors.toCollection(HashSet::new)));
+    t.put(sink, Stream.of(sink).collect(Collectors.toCollection(HashSet::new)));
+    f.put(sink, sink.getWeight());
+
+    for (NodeDTO currentNode : reversedTopologicalSort) {
+      final List<NodeDTO> successors = graph.getAdjNodes(currentNode);
+      if (successors.size() > 0) {
+        if (currentNode.isBeginCondition()) {
+          // todo: implement
+          // per ogni successor v devo calcolare il C(S(v))
+          // prendere il v che totalizza il C(S(v)) massimo
+
+          // riga 9
+          final NodeDTO vStar = successors.stream()
+                  .max(Comparator.comparing(s1 -> computeWCET(s.get(s1))))
+                  .orElse(null);
+
+          // riga 10
+          Set<NodeDTO> tmpSet = new HashSet<>();
+          tmpSet.add(currentNode);
+          tmpSet.addAll(s.get(vStar));
+
+          s.put(currentNode, tmpSet);
+
+          // riga 11
+          final NodeDTO uStar = successors.stream()
+                  .max(Comparator.comparing(f::get))
+                  .orElse(null);
+
+          // riga 12
+          tmpSet = new HashSet<>();
+          tmpSet.add(currentNode);
+          tmpSet.addAll(t.get(uStar));
+
+          t.put(currentNode, tmpSet);
+
+          // riga 13
+          f.put(currentNode, currentNode.getWeight() + f.get(uStar));
+
+        } else {
+          // todo: implement
+
+          // riga 15
+          Set<NodeDTO> tmpSet = new HashSet<>();
+          successors.stream()
+                  .map(s::get)
+                  .forEach(tmpSet::addAll);
+
+          tmpSet.add(currentNode);
+
+          s.put(currentNode, tmpSet);
+
+          // riga 16-17
+          final NodeDTO uStar = successors.stream()
+                  .max(Comparator.comparing(u1 -> {
+
+                    Integer summation = 0;
+                    for (NodeDTO w : successors) {
+                      if (!w.equals(u1)) {
+                        summation += (computeWCET(s.get(w)) - u1.getWeight()) / m;
+                      }
+                    }
+
+                    return f.get(u1) + summation;
+
+                  }))
+                  .orElse(null);
+
+          // riga 18
+          tmpSet = new HashSet<>();
+          tmpSet.add(currentNode);
+          tmpSet.addAll(t.get(uStar));
+
+          t.put(currentNode, tmpSet);
+
+          // riga 19
+          Integer summation = 0;
+          for (NodeDTO w : successors) {
+            if (uStar != null && !w.equals(uStar)) {
+              summation += (computeWCET(s.get(w)) - uStar.getWeight()) / m;
+            }
+          }
+
+          f.put(currentNode, currentNode.getWeight() + f.get(uStar) + summation);
+
+        }
+      }
+    }
+    return f.get(source);
+  }
 }
